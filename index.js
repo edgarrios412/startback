9a102eff0e71e39b595632c99e7c73c76a963259
unknown <edgarrios412@gmail.com>
2024-12-06 14:29:56 -0500

    push

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cron = require("node-cron")
 
 cron.schedule("*/10 * * * *", async () => {
     console.log("Pasaron 10 minutos")
-    await axios.get("https://natalieback.onrender.com/user/")
-    await axios.get("https://burgermake.onrender.com/user/")
+    await axios.get("https://fideliza-back.onrender.com/user/")
     console.log("Peticiones realizadas")
 })
