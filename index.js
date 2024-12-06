97a6930a4a71975091ef60840d03f2c52ab31c40
unknown <edgarrios412@gmail.com>
2024-12-06 14:35:15 -0500

    push

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const { default: axios } = require("axios")
 const cron = require("node-cron")
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const morgan = require("morgan")
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
+app.get("/", (req, res) => {
+    res.json({activated:true})
+})
 
 cron.schedule("*/10 * * * *", async () => {
     console.log("Pasaron 10 minutos")
